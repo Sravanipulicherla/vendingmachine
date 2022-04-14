941136d01666fa05db80358c5b375c0a75f92dd5
Sravanipulicherla <pulisravani21@gmail.com>
2022-04-14 18:38:57 +0200

    First commit for vendingmachine

---
+++ Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vendingmachine
+{
+    internal class Products
+    {
+        public string price = "10kr";
+        public string info = "100ml";
+        public string name = "";
+        public string usage = "you can drink it";
+        public int count = 0;
+    }
+
+    class CoolDrinks : Products
+    { 
+
+    }
+    class FruitJuice : Products
+    { 
+    
+    }
+    class Chocolates : Products
+    {
+    
+    }
+}
